9cc9352197aabd77d36bdc4b465d696d461b8864
Rahul <rahulee19@gmail.com>
2022-02-18 00:23:50 +0530

    assignment

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
